4daddda8eba82d243eb70c73b1c72afc3c45a643
neni5 <sheth.niral9@gmail.com>
2020-11-30 12:05:00 +0530

    Initial commit

---
+++ src/main/java/junit/MathCal.java
@@ -0,0 +1,8 @@
+package junit;
+
+public class MathCal {
+
+	public double addition(double a, double b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/junit/MathCalTest.java
@@ -0,0 +1,17 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathCalTest {
+
+	/**
+	 * Test method for {@link junit.MathCal#addition(double, double)}.
+	 */
+	@Test
+	void testAddition() {
+		System.out.println("This is test method for addition method.");
+	}
+
+}
